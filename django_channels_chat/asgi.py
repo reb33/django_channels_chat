ba0cf4478c78ad050ab77b17cd4cff65026c6da6
k.barylnikov <ipt_git_bot@ipt.ru>
2026-03-25 23:24:46 +0300

    connect с websocket от клиента, консюмер на сервере, подтверждение рукопожатия, безопасность AllowedHostsOriginValidator и AuthMiddlewareStack

--- django_channels_chat/asgi.py
+++ django_channels_chat/asgi.py
@@ -9,8 +9,24 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels_chat.settings')
 
-application = get_asgi_application()
+asgi_application = get_asgi_application()
+
+import chat.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": asgi_application,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(chat.routing.websocket_urlpatterns)
+            ),
+        )
+    }
+)
